a6a176787eeebe545469458b1b467111b9196bcf
tigerfarm <tigerfarm@gmail.com>
2019-07-24 12:13:47 -0700

    updates

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-<html lang="en">
-    <head>
-        <title>Tiger Farm Press v1</title>
-        <META HTTP-EQUIV="Refresh" CONTENT="0; URL=docroot/index.html">
-    </head>
-    <body>
-    </body>
-</html>
+<?php
+echo exec('node webserver.js');
+?>
---
+++ work/index.php
@@ -0,0 +1,8 @@
+<html lang="en">
+    <head>
+        <title>Tiger Farm Press v1</title>
+        <META HTTP-EQUIV="Refresh" CONTENT="0; URL=docroot/index.html">
+    </head>
+    <body>
+    </body>
+</html>
