3c5ecfe89ba2164a2c40f03db4b285ae13263fc1
tigerfarm <tigerfarm@gmail.com>
2019-07-24 11:43:54 -0700

    updates

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html lang="en">
     <head>
-        <title>Tiger Farm Press v1</title>
+        <title>Tiger Farm Press</title>
         <META HTTP-EQUIV="Refresh" CONTENT="0; URL=docroot/index.html">
     </head>
     <body>
